9d0c4d60e904214f29539fe233962ca4018527d2
Bliaxx <67436831+Bliaxx@users.noreply.github.com>
2024-05-02 17:41:35 +0200

    Add "InsertAtFront" function + operator<< for linked list

--- Simple Linked List/examples/main.cpp
+++ Simple Linked List/examples/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include "LinkedList.h"
 
 int main()
 {
+	LinkedList<int> myLinkedList;
+	myLinkedList.InsertAtFront(10);
+	myLinkedList.InsertAtFront(20);
+	myLinkedList.InsertAtFront(30);
+	std::cout << myLinkedList;
 }
 
--- Simple Linked List/include/LinkedList.h
+++ Simple Linked List/include/LinkedList.h
@@ -1,5 +1,51 @@
 #pragma once
+
+#include "Node.h"
+
+template<typename T>
 class LinkedList
 {
+	Node<T>* head = nullptr;
+	Node<T>* tail = nullptr;
+
+public:
+	LinkedList() = default;
+	~LinkedList()
+	{
+		Node<T>* _currentNode = head;
+		while (_currentNode != nullptr)
+		{
+			Node<T>* _tempNodeToDelete = _currentNode;
+			_currentNode = _currentNode->GetNextNode();
+			delete _tempNodeToDelete;
+		}
+	}
+
+public:
+	void InsertAtFront(const T& _value)
+	{
+		Node<T>* _newNode = new Node<T>(_value);
+		if (!head)
+		{
+			head = _newNode;
+			tail = _newNode;
+		}
+		else
+		{
+			_newNode->SetNextNode(head);
+			head = _newNode;
+		}
+	}
+
+	friend std::ostream& operator<<(std::ostream& _os, const LinkedList<T>& _list)
+	{
+		Node<T>* _currentNode = _list.head;
+		while (_currentNode != nullptr)
+		{
+			_os << _currentNode->GetData() << "->";
+			_currentNode = _currentNode->GetNextNode();
+		}
+		return _os;
+	}
 };
 
