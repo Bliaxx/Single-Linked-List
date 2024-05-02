6125df33ff1be16e1196c25a69ab7e8c30833f98
Bliaxx <67436831+Bliaxx@users.noreply.github.com>
2024-05-02 14:07:27 +0200

    Add Node class

---
+++ Simple Linked List/Node.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <iostream>
+
+template<typename T>
+class Node
+{
+	T data;
+	Node<T>* nextNode = nullptr;
+
+public:
+	Node(): data(T()) {}
+	Node(const T& _value) : data(_value) {}
+
+public:
+	inline T GetData() const { return data; }
+	inline void SetData(const T& _value) { data = _value; }
+	inline Node<T>* GetNextNode() const { return nextNode; }
+	inline void SetNextNode(Node<T>* _nextNode) { nextNode = _nextNode; }
+
+	inline bool operator==(const Node<T>& _otherNode) const { return data == _otherNode.data; }
+	inline bool operator!=(const Node<T>& _otherNode) const { return data != _otherNode.data; }
+	friend std::ostream& operator<<(std::ostream& _os, const Node<T>& _node)
+	{
+		_os << "Node(data : " << _node.data << ")";
+		return _os;
+	}
+
+};
